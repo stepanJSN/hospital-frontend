b206a3165bc524352a60055407cf8eb9fdfb51e9
Stepan Stadniuk <stepanigcom@gmail.com>
2024-07-04 15:04:13 +0300

    prepared requests for information about the doctor and for making an appointment

--- src/services/appointment.ts
+++ src/services/appointment.ts
@@ -1,4 +1,4 @@
-import { GetDoctorsType, IDoctors, ISpecialization } from "@/types/appointment.type";
+import { GetDoctorsType, IAppointmentPayload, IAvailableTime, IDoctor, IDoctorShort, ISpecialization } from "@/types/appointment.type";
 import { axiosWithAuth } from "./api";
 
 class AppointmentService {
@@ -7,13 +7,31 @@ class AppointmentService {
 	};
 
 	async getDoctors(data: GetDoctorsType) {
-		return (await axiosWithAuth.get<IDoctors[]>('/staff', 
+		return (await axiosWithAuth.get<IDoctorShort[]>('/staff', 
 			{ params: { 
 				specializationId: data.specializationId,
 				date: data.date
 			}}
 		)).data;
 	};
+
+	async getDoctorById(id: string) {
+		return (await axiosWithAuth.get<IDoctor>(`/staff/${id}`)).data;
+	}
+
+	async getAvailableTime(staffId: string, startDate: string, endDate: string) {
+		return (await axiosWithAuth.get<IAvailableTime[]>('/staff/schedule', 
+			{ params: { 
+				staffId,
+				startDate,
+				endDate,
+			}}
+		)).data;
+	}
+
+	async makeAppointment(data: IAppointmentPayload) {
+		await axiosWithAuth.post('/appointments', data);
+	}
 }
 
 export const appointmentService = new AppointmentService();
--- src/types/appointment.type.ts
+++ src/types/appointment.type.ts
@@ -3,18 +3,37 @@ export interface ISpecialization {
   title: string
 }
 
-export interface IDoctors {
-  id: string
-  name: string
-  surname: string
-  experience: number
-  gender: 'male' | 'female'
+export interface IDoctor {
+  email: string;
+  name: string;
+  surname: string;
+  telephone: string;
+  gender: 'male' | 'female';
+  experience: number;
+  description: string;
   specialization: {
-    title: string;
-  }
+      title: string;
+  };
+}
+
+export interface IDoctorShort extends Omit<IDoctor, "email" | "telephone" | "description"> {
+  id: string;
 }
 
 export type GetDoctorsType = {
   specializationId: string; 
   date?: string
+}
+
+export interface IAvailableTime {
+  dayOfWeek: number;
+  startTime: number;
+  endTime: number;
+  bookedTime: number[];
+}
+
+export interface IAppointmentPayload {
+  staffId: string;
+  customerId: string;
+  dateTime: string;
 }
