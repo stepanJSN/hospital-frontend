7cce9126cba7741ecf16169a46f6c8c51399e42c
Stepan Stadniuk <stepanigcom@gmail.com>
2024-08-27 16:56:15 +0300

    Refactored customer page

--- src/app/(cabinet)/customers/[id]/page.tsx
+++ src/app/(cabinet)/customers/[id]/page.tsx
@@ -1,61 +1,15 @@
-"use client"
 
-import Notification from "@/components/Notifications";
 import { customerService } from "@/services/customer";
-import { Avatar, Box, Skeleton, Typography } from "@mui/material";
-import { useQuery } from "@tanstack/react-query";
-import dayjs from "dayjs";
-import { useParams } from "next/navigation";
+import { QueryClient } from "@tanstack/react-query";
+import CustomerPage from "./CustomerPage";
 
-export default function Customer() {
-  const { id } = useParams<{ id: string }>();
+export default async function Customer({ params }: { params: { id: string } }) {
+  const queryClient = new QueryClient();
 
-  const { data, isFetching, isError } = useQuery({
-    queryKey: ['customerId', id],
-    queryFn: () => customerService.get(id),
-    enabled: !!id,
+  await queryClient.prefetchQuery({
+    queryKey: ['customerId', params.id],
+    queryFn: () => customerService.get(params.id),
   })
 
-  return (
-    <>
-      <Box display="flex" margin={3} width="100%">
-        <Box>
-          {(!isFetching && !isError) && <Avatar 
-            alt="Avatar"
-            src={data?.avatarUrl}
-            sx={{ width: 150, height: 150 }}
-          />}
-          {(isFetching || isError) && <Skeleton variant="circular" width={150} height={150} />}
-        </Box>
-        <Box marginLeft={3}>
-          {(!isFetching && !isError) && <>
-            <Typography 
-              component="h1"
-              variant="h4"
-              paddingBottom={2}
-              paddingTop={2}
-            >{`${data?.name} ${data?.surname}`}</Typography>
-            <Typography>{'Email: ' + data?.email}</Typography>
-            <Typography>{'Telephone: ' + data?.telephone}</Typography>
-            <Typography>{'Gender: ' + data?.gender}</Typography>
-            <Typography>{'Gender: ' + dayjs(data?.birthday).format('DD.MM.YYYY')}</Typography>
-          </>}
-          {(isFetching || isError) && <>
-            <Skeleton component="h4" width={200} />
-            <Skeleton width={200} />
-            <Skeleton width={200} />
-            <Skeleton width={200} />
-            <Skeleton width={200} />
-          </>}
-        </Box>
-      </Box>
-      <Notification 
-        trigger={isError}
-        position={{ 
-          horizontal: 'center',
-          vertical: 'top'
-        }}
-      />
-    </>
-  )
+  return <CustomerPage id={params.id} />
 }
