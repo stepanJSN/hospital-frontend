ff643aedb5b6529036ba5fe777e5a6239fadb8c4
Stepan Stadniuk <stepanigcom@gmail.com>
2024-08-30 11:20:27 +0300

    Fixed bug with skeleton, added useClient directive for Doctor info component

--- src/app/(cabinet)/staff/[id]/DoctorInfo.tsx
+++ src/app/(cabinet)/staff/[id]/DoctorInfo.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import Notification from "@/components/Notifications";
 import useGetDoctors from "@/hooks/useGetDoctorsData";
 import { Avatar, Box, Divider, Skeleton, Typography } from "@mui/material";
@@ -9,44 +11,49 @@ type DoctorInfoProps = {
 }
 
 export default function DoctorInfo({ staffId, isAdmin }: DoctorInfoProps) {
-  const { doctorData, isFetching, isError } = useGetDoctors(staffId);
+  const { doctorData, isError } = useGetDoctors(staffId);
 
   return (
     <>
       <Box display="flex" margin={3} width="100%">
         <Box>
-          {doctorData && <Avatar 
-            alt="Avatar"
-            src={doctorData.avatarUrl}
-            sx={{ width: 150, height: 150 }}
-          />}
-          {(isFetching || isError) && <Skeleton variant="circular" width={150} height={150} />}
+          {doctorData
+            ? <Avatar 
+                alt="Avatar"
+                src={doctorData.avatarUrl}
+                sx={{ width: 150, height: 150 }}
+              />
+            : <Skeleton variant="circular" width={150} height={150} />
+          }
         </Box>
         <Box marginLeft={3} flex="auto">
-          {doctorData && 
-          <>
-            <Typography 
-              component="h1"
-              variant="h4"
-              paddingBottom={2}
-              paddingTop={2}
-            >{`${doctorData.name} ${doctorData.surname}`}</Typography>
-            <Typography>{'Specialization: ' + doctorData.specialization?.title}</Typography>
-            <Typography>{'Experience: ' + doctorData.experience + ' year'}</Typography>
-            <Typography>{'Email: ' + doctorData.email}</Typography>
-            <Typography>{'Telephone: ' + doctorData.telephone}</Typography>
-            <Typography>{'Gender: ' + doctorData.gender}</Typography>
-            <Typography>{'Room: ' + doctorData.room}</Typography>
-          </>}
-          {(isFetching || isError) && <>
-            <Skeleton component="h4" width={200} />
-            <Skeleton width={200} />
-            <Skeleton width={200} />
-            <Skeleton width={200} />
-            <Skeleton width={200} />
-            <Skeleton width={200} />
-            <Skeleton width={200} />
-          </>}
+          {doctorData 
+          ? 
+            <>
+              <Typography 
+                component="h1"
+                variant="h4"
+                paddingBottom={2}
+                paddingTop={2}
+              >{`${doctorData.name} ${doctorData.surname}`}</Typography>
+              <Typography>{'Specialization: ' + doctorData.specialization?.title}</Typography>
+              <Typography>{'Experience: ' + doctorData.experience + ' year'}</Typography>
+              <Typography>{'Email: ' + doctorData.email}</Typography>
+              <Typography>{'Telephone: ' + doctorData.telephone}</Typography>
+              <Typography>{'Gender: ' + doctorData.gender}</Typography>
+              <Typography>{'Room: ' + doctorData.room}</Typography>
+            </>
+          : 
+            <>
+              <Skeleton component="h4" width={200} />
+              <Skeleton width={200} />
+              <Skeleton width={200} />
+              <Skeleton width={200} />
+              <Skeleton width={200} />
+              <Skeleton width={200} />
+              <Skeleton width={200} />
+            </>
+          }
         </Box>
         {isAdmin &&
           <Box 
@@ -61,8 +68,10 @@ export default function DoctorInfo({ staffId, isAdmin }: DoctorInfoProps) {
         }
       </Box>
       <Divider sx={{ color: 'grey' }} />
-      {doctorData && <Typography padding={2}>{doctorData.description ?? 'Description not provided'}</Typography>}
-      {(isFetching || isError) && <Skeleton width="100%" height={100} />}
+      {doctorData 
+        ? <Typography padding={2}>{doctorData.description ?? 'Description not provided'}</Typography>
+        : <Skeleton width="100%" height={100} />
+      }
       <Notification 
         trigger={isError}
         position={{ vertical: 'top', horizontal: 'center' }}
