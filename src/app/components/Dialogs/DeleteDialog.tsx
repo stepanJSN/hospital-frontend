9f424951ce52707f4aec88ab06ae24841a922869
Stepan Stadniuk <stepanigcom@gmail.com>
2024-09-06 10:26:18 +0300

    + adaptation of delete dialog

--- src/app/components/Dialogs/DeleteDialog.tsx
+++ src/app/components/Dialogs/DeleteDialog.tsx
@@ -35,12 +35,19 @@ export default function DeleteDialog({
       aria-labelledby="alert-dialog-title"
       aria-describedby="alert-dialog-description"
     >
-      <DialogTitle id="alert-dialog-title">
+      <DialogTitle
+        id="alert-dialog-title"
+        sx={{
+          p: { xs: '13px', md: '16px 24px' },
+          fontSize: { xs: '1.2rem', md: '1.25rem' },
+          lineHeight: { xs: '1.5', md: '1.6' },
+        }}
+      >
         {title ?? 'Are you sure you want to perform this action?'}
         {isError && <Alert severity="error">Error. Try again</Alert>}
         {isLoading && <LinearProgress />}
       </DialogTitle>
-      <DialogContent>
+      <DialogContent sx={{ p: { xs: '13px', md: '16px 24px' } }}>
         <DialogContentText id="alert-dialog-description">
           {content ?? 'This action cannot be undone. All data will be lost'}
         </DialogContentText>
