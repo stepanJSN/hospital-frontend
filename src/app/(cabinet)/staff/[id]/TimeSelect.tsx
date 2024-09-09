90a3ce89a2603a5fae22733594ff21623b6f8701
Stepan Stadniuk <stepanigcom@gmail.com>
2024-09-09 14:08:55 +0300

    + component to select time on small screens

---
+++ src/app/(cabinet)/staff/[id]/TimeSelect.tsx
@@ -0,0 +1,43 @@
+import { Button, Dialog, DialogContent, DialogTitle } from '@mui/material';
+
+type TimeSelectProps = {
+  handleClose: () => void;
+  handleDateSelect: (time: number, date: number) => void;
+  selectedDate: {
+    availableTime: number[];
+    date: number;
+  } | null;
+};
+
+export default function TimeSelect({
+  handleClose,
+  selectedDate,
+  handleDateSelect,
+}: TimeSelectProps) {
+  return (
+    <Dialog
+      open={!!selectedDate}
+      onClose={handleClose}
+      aria-labelledby="select-time-dialog"
+    >
+      <DialogTitle id="select-time-dialog">
+        {selectedDate &&
+          new Date(selectedDate.date).toLocaleDateString() + ' | Select time'}
+      </DialogTitle>
+      <DialogContent
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+        }}
+      >
+        {selectedDate?.availableTime.map((time) => (
+          <Button
+            key={time}
+            onClick={() => handleDateSelect(time, selectedDate.date)}
+          >{`${time}:00`}</Button>
+        ))}
+      </DialogContent>
+    </Dialog>
+  );
+}
